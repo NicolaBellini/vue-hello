9e6526de34358c95c8f6f394fd6568da75cb98a0
NicolaBellini <bellininicola4366@gmail.com>
2024-03-11 15:25:09 +0100

    [add] img with dadta ( )

--- js/script.js
+++ js/script.js
@@ -3,19 +3,21 @@ const {createApp} = Vue;
 createApp({
   data() {
     return {
-      message:'Un passo alla Vuelta',
+      message:'Un passo alla Vuelta!',
       tempMessage:'',
-      counter:0,
+      img:'url(https://unsplash.com/it/foto/un-primo-piano-di-un-uccello-con-la-testa-rossa-BUhVFtY-890)'
     }
   },
   methods: {
     changer(string){
       this.message = string;
     },
+    applyBackImg(){
 
+    }
+ 
   },
   mounted() {
     console.log('ciao');
-
   },
 }).mount('#app')
