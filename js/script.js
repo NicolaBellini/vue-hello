4faf53d89dfb723107ddd9af491c31a9ae6d4202
NicolaBellini <bellininicola4366@gmail.com>
2024-03-11 15:57:16 +0100

    [add] blurred bg

--- js/script.js
+++ js/script.js
@@ -5,19 +5,23 @@ createApp({
     return {
       message:'Un passo alla Vuelta!',
       tempMessage:'',
-      image:"assets/sfondo.jpg"
+      image:"assets/sfondo.jpg",
+      blurredClass: 'blurred',
+      isFocus: false
     }
   },
   methods: {
     changer(string){
       this.message = string;
     },
-    applyBackImg(){
+    focussed(){
+      this.isFocus = true
+    },
 
-    }
- 
   },
   mounted() {
     console.log('ciao');
-  },
-}).mount('#app')
+    this.input = document.querySelector('.my-input');
+    console.log(this.input);
+  }
+}).mount('#app');
