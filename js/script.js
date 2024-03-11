6e61ccae354c5490f072c84a1edda08cf2de79b5
NicolaBellini <bellininicola4366@gmail.com>
2024-03-11 14:28:09 +0100

    [add] button and changer( )

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@ const {createApp} = Vue;
 createApp({
   data() {
     return {
-      message:'Hello World!',
+      message:'Un passo alla Vuelta',
+      tempMessage:'',
     }
   },
   methods: {
-    
+    changer(string){
+      this.message = string;
+    },
   },
   mounted() {
     console.log('ciao');
+
   },
 }).mount('#app')
