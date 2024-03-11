60870eb87bbc0e681e97602f32e0bfe9e1df5632
NicolaBellini <bellininicola4366@gmail.com>
2024-03-11 14:38:55 +0100

    [add] counter

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ createApp({
     return {
       message:'Un passo alla Vuelta',
       tempMessage:'',
+      counter:0,
     }
   },
   methods: {
     changer(string){
       this.message = string;
     },
+
   },
   mounted() {
     console.log('ciao');
